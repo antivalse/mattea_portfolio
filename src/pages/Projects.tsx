f6e13587ac1fc180dd8d09e944f4bb20ea551e0d
Mattea <mattea.sax@gmail.com>
2025-03-11 19:05:42 +0100

    added more projects to projects array and render on projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,45 +2,66 @@
 
 import BackToHomepage from "../components/navigation/BackToHomepage";
 import scrollToDiv from "../helpers/scrollToDiv";
-import CandyShopImg from "../assets/images/project-images/candyshopimg.png";
 
 import { useEffect } from "react";
 
 const ProjectsPage = () => {
-  const projectImages = [
-    {
-      id: 1,
-      title: "Bortakväll - online candy shop",
-      src: "../assets/images/candyshopimg.png",
-    },
-  ];
   useEffect(() => {
     scrollToDiv("projects-content");
   }, []);
+
+  const projectImages = [
+    {
+      id: 1,
+      description: "Collaboration project - html | css | javascript",
+      href: "https://deft-flan-8c51cf.netlify.app/",
+      src: "src/assets/images/project-images/candyshopimg.png",
+      title: "Bortakväll - Online Candy Shop",
+    },
+    {
+      id: 2,
+      description: "School assignment - html | css | javascript",
+      href: "https://gleeful-bunny-50bbaf.netlify.app/",
+      src: "src/assets/images/project-images/guessinggameimg.png",
+      title: "Guess the Name Game - Quiz Game",
+    },
+    {
+      id: 3,
+      description: "School assignment - html | css | react",
+      href: "https://incredible-squirrel-e5e5fe.netlify.app/",
+      src: "src/assets/images/project-images/tmdbimg.png",
+      title: "The Movie Database",
+    },
+  ];
+
   return (
-    <div id="projects" className="page-container projects">
-      <h1 className="header header--page">projects</h1>
-      <ul id="projects-content">
-        {projectImages.map((project) => (
-          <li
-            key={project.id}
-            className="projects__card flex flex-col justify-between gap-5"
-          >
-            <p>{project.title}</p>
-            <img src={CandyShopImg} alt={project.title} />
-            <div className="text-right">
-              <a
-                className="self-end"
-                href="https://bortakvall.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                View Project →
+    <div className="page-container projects">
+      <h1 className="header header--page mb-10">projects</h1>
+      <div id="projects-content">
+        <ul className="flex gap-5 mt-20">
+          {projectImages.map((project) => (
+            <li
+              key={project.id}
+              className="projects__card flex flex-col justify-between gap-5"
+            >
+              <p>{project.title}</p>
+              <a href={project.href}>
+                <img src={project.src} alt={project.title} />
               </a>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="text-right">
+                <a
+                  className="projects__card__link-btn self-end"
+                  href={project.href}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  View Project →
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
       <BackToHomepage />
     </div>
   );
